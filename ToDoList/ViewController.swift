575d00e5c50707728fc514c5718b84d11fd6f289
Dmitry grinev <grinev.dmt@gmail.com>
2020-11-12 15:42:29 +0500

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Dmitry Grinev on 12.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
